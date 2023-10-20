d72fe6880a7c5b26bbc74c977ce4f0abc81a952e
code-bullett <127530534+code-bullett@users.noreply.github.com>
2023-10-20 10:13:36 -0300

    added the getUrlFromHtml to find links in html and returning the absolute path

--- crawl.js
+++ crawl.js
@@ -1,3 +1,27 @@
+const {JSDOM} = require('jsdom')
+
+function getUrlFromHtml(htmlBody,baseUrl){
+    const url = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for(const linkElement of linkElements){
+        if(linkElement.href.slice(0,1) === '/'){
+            try{
+                const tst = new URL(baseUrl+linkElement.href)
+                url.push(tst.href)
+            }catch(err){
+            }
+        }else{
+            try{
+                const tst = new URL(linkElement.href)
+                url.push(tst.href)
+            }catch(err){
+            }
+        }
+    }
+    return url
+}
+
 function normalizeUrl(urlstring) {
   const url = new URL(urlstring);
   const hostpath = `${url.hostname}${url.pathname}`
@@ -8,5 +32,6 @@ function normalizeUrl(urlstring) {
 };
 
 module.exports = {
-  normalizeUrl
+    normalizeUrl,
+    getUrlFromHtml
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeUrl } = require('./crawl.js');
+const { normalizeUrl, getUrlFromHtml } = require('./crawl.js');
 const { test, expect } = require('@jest/globals')
 
 
@@ -9,9 +9,87 @@ test('normalizeUrl strip protocal', () => {
   expect(returned).toEqual(expected)
 })
 
-test('normalizeUrl strip protocal', () => {
+test('normalizeUrl strip trailing slash', () => {
   const input = "https://blog.boot.dev/path/"
   const returned = normalizeUrl(input)
   const expected = "blog.boot.dev/path"
   expect(returned).toEqual(expected)
+})
+
+test('normalizeUrl strip uppercase', () => {
+  const input = "https://BLOG.boot.dev/path"
+  const returned = normalizeUrl(input)
+  const expected = "blog.boot.dev/path"
+  expect(returned).toEqual(expected)
+})
+
+
+test('Find links from Htlm', () => {
+  const input = `
+    <html>
+        <body>
+          <a href = "https://blog.boot.dev/">
+               Boot.dev blog
+            </a>
+        </body>
+    </html>
+    `
+    const inputBaseUrl = "https://blog.boot.dev"
+    const returned = getUrlFromHtml(input,inputBaseUrl)
+    const expected = ["https://blog.boot.dev/"]
+    expect(returned).toEqual(expected)
+})
+
+test('Find links from Htlm, relative url', () => {
+  const input = `
+    <html>
+        <body>
+          <a href = "/path/">
+               Boot.dev blog
+            </a>
+        </body>
+    </html>
+    `
+    const inputBaseUrl = "https://blog.boot.dev"
+    const returned = getUrlFromHtml(input,inputBaseUrl)
+    const expected = ["https://blog.boot.dev/path/"]
+    expect(returned).toEqual(expected)
+})
+
+test('Find links from Htlm, both', () => {
+  const input = `
+    <html>
+        <body>
+          <a href = "/path/">
+               Boot.dev blog
+            </a>
+          <a href = "/path4/">
+               Boot.dev blog
+            </a>
+          <a href = "https://blog.boot.dev/">
+               Boot.dev blog
+            </a>
+        </body>
+    </html>
+    `
+    const inputBaseUrl = "https://blog.boot.dev"
+    const returned = getUrlFromHtml(input,inputBaseUrl)
+    const expected = ["https://blog.boot.dev/path/","https://blog.boot.dev/path4/","https://blog.boot.dev/"]
+    expect(returned).toEqual(expected)
+})
+
+test('Find links from Htlm, invalid', () => {
+  const input = `
+    <html>
+        <body>
+          <a href = "invalid">
+                invalid
+            </a>
+        </body>
+    </html>
+    `
+    const inputBaseUrl = "https://blog.boot.dev"
+    const returned = getUrlFromHtml(input,inputBaseUrl)
+    const expected = []
+    expect(returned).toEqual(expected)
 })
